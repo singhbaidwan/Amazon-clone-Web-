aa75b5d855a9a579c62a5f8ca522750e29f6e0d1
Dalveer Singh <59986053+singhbaidwan@users.noreply.github.com>
2023-01-02 16:53:52 +0530

    added context api to handle add to basket

--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -3,7 +3,11 @@ import "./Header.css";
 import SearchIcon from "@mui/icons-material/Search";
 import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
 import { Link } from "react-router-dom";
+import { useStateValue } from "./StateProvider";
+
 function Header() {
+  const [{ basket }, dispatch] = useStateValue();
+
   return (
     <div className="header">
       <Link to="/">
@@ -33,7 +37,9 @@ function Header() {
         <Link to="/checkout">
           <div className="header--basketOption">
             <ShoppingBasketIcon />
-            <span className="header--optionLineTwo header--basketCount">0</span>
+            <span className="header--optionLineTwo header--basketCount">
+              {basket?.length}
+            </span>
           </div>
         </Link>
       </div>
